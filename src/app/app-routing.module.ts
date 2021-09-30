1bd7b5d93549008a2bd19749a058a503b9f50c32
alydw <wdyla2001@hotmail.com>
2021-09-30 11:33:23 -0300

    categoria delete implementado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoriaCreateComponent } from './components/view/categoria/categoria-create/categoria-create.component';
+import { CategoriaDeleteComponent } from './components/view/categoria/categoria-delete/categoria-delete.component';
 import { CategoriaReadComponent } from './components/view/categoria/categoria-read/categoria-read.component';
 import { HomeComponent } from './components/view/home/home.component';
 
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'categorias/create',
     component: CategoriaCreateComponent
+  },
+  {
+    path: 'categorias/delete:id',
+    component: CategoriaDeleteComponent
   }
 ];
 
